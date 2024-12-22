1e96c88117424d44a25fc9a70e2049087d67a422
Anton Serdyuchenko <anton415@gmail.com>
2024-12-22 15:40:37 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Anton Serdyuchenko on 22.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
